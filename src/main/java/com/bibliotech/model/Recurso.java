42fe8c2775dbf68e60bf913939fdadf6729b4d51
SantiQR-UM <sr.quiroga@alumno.um.edu.ar>
2026-04-25 19:54:07 -0300

    feature: Libros y Ebooks

--- src/main/java/com/bibliotech/model/Recurso.java
+++ src/main/java/com/bibliotech/model/Recurso.java
@@ -4,5 +4,3 @@ public interface Recurso {
     String titulo();
 }
 
-// src/com/bibliotech/model/Libro.java
-public record Libro(String isbn, String titulo, String autor, int paginas) implements Recurso {}
